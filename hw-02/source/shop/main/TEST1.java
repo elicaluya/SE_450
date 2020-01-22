5273f4846a6fb7acc896ed187e2a5246160e7d53
Elijah Caluya <elicaluya@yahoo.com>
2020-01-22 11:29:43 -0800

    finishing testing

--- hw-02/source/shop/main/TEST1.java
+++ hw-02/source/shop/main/TEST1.java
@@ -39,5 +39,40 @@ public class TEST1 extends TestCase {
     expect(v1,"Title1 (2000) : Director1 [10,0,0]");
     
     // TODO  
+    // Create new Video object
+    Video v2 = Data.newVideo("A", 2001, "B");
+    Assert.assertTrue(Data.newAddCmd(_inventory, v2, 3).run());
+    Assert.assertEquals(_inventory.size(), 2);
+    Assert.assertEquals(_inventory.get(v2).numOwned(), 3);
+    expect(v2,"A (2001) : B [3,0,0]");
+    
+    // Check out v1 and v2
+    Assert.assertTrue(Data.newOutCmd(_inventory, v2).run());
+    Assert.assertTrue(Data.newOutCmd(_inventory, v1).run());
+    Assert.assertEquals(_inventory.get(v2).numOut(), 1);
+    Assert.assertEquals(_inventory.get(v1).numOut(), 1);
+    Assert.assertEquals(_inventory.get(v2).numRentals(), 1);
+    Assert.assertEquals(_inventory.get(v1).numRentals(), 1);
+    expect(v2,"A (2001) : B [3,1,1]");
+    expect(v1,"Title1 (2000) : Director1 [10,1,1]");
+    
+    // Check out v2 again
+    Assert.assertTrue(Data.newOutCmd(_inventory, v2).run());
+    Assert.assertEquals(_inventory.get(v2).numOut(), 2);
+    Assert.assertEquals(_inventory.get(v2).numRentals(), 2);
+    expect(v2,"A (2001) : B [3,2,2]");
+
+    
+    // Add more v2 to the inventory
+    Assert.assertTrue(Data.newAddCmd(_inventory, v2, 3).run());
+    Assert.assertEquals(_inventory.get(v2).numOwned(), 6);
+    expect(v2,"A (2001) : B [6,2,2]");
+
+    // Check in v2 back into the inventory
+    Assert.assertTrue(Data.newInCmd(_inventory, v2).run());
+    Assert.assertEquals(_inventory.get(v2).numOut(), 1);
+    expect(v2,"A (2001) : B [6,1,2]");
+
+    
   }
 }
